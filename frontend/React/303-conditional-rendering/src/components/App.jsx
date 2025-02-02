71cde7bd80663524aa9efc377492fa53119f44bc
Karan Purohit <karanvpurohit@gmail.com>
2025-02-02 10:00:17 +0530

    Modified

--- frontend/React/303-conditional-rendering/src/components/App.jsx
+++ frontend/React/303-conditional-rendering/src/components/App.jsx
@@ -6,8 +6,7 @@ const currentTime = new Date().getHours();
 function App() {
   return (
     <div className="container">
-      {/* {(isLoggedIn && currentTime > 12) ? <h1>Hello</h1> : <Login />} */}
-      { currentTime <= 12 && <h1>Hello</h1>}
+      {(isLoggedIn && currentTime > 12) ? <h1>Hello</h1> : <Login />}x
     </div>
   );
 }
